1d4c2020259309afcdbe924b989557c0f5e5037b
kon78 <misterqjr@mail.ru>
2025-02-21 19:25:42 +0300

    editing file

--- Lab_1/task_2_3/task2.h
+++ Lab_1/task_2_3/task2.h
@@ -1,5 +1,5 @@
 #ifndef TASK2_H
 #define TASK2_H
   float SpeedPoint1, SpeedPoint2, TimePoint1, TimePoint2, AccelerationAll;//task 2 вторая задача
-  bool BoolEnterDataTimePoint1,BoolEnterDataTimePoint2,BoolEnterDataTimeSpeedPoint1,BoolEnterDataTimeSpeedPoint2;
+//  bool BoolEnterDataTimePoint1,BoolEnterDataTimePoint2,BoolEnterDataTimeSpeedPoint1,BoolEnterDataTimeSpeedPoint2;
 #endif // TASK2_H
