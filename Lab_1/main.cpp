c9b56d4f2cbf76a8b93233f3499dfaaa8edd18d3
kon78 <misterqjr@mail.ru>
2025-03-02 22:08:26 +0300

    added task 3 to main

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -100,4 +100,187 @@ int main(int argc, char *argv[])
 #endif
 
 #if 1
+//директивы препроцессору
+#include <iostream>//поточный модуль
+#include <stdio.h>
+#include <math.h>//для математики
+
+//используем пространство имен std, можно не писать запись std::cout
+using namespace std;
+
+// функция для проверки вводимых данных \
+  можно разместить перед main()
+
+//[тип float] [имя функции] (параметры передаваемые в функцию) \
+  сейчас их нет, void
+float EnterData(void){
+  float AnyValue;
+/*
+   * проверка ввода считается достаточно ответственой и требовательной частью программы
+   * лучше всего подошли бы regex-регулярные выражения, поиск запрещенных символов
+   * /,a-z A-Z/
+*/
+  while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
+    {
+      cout << "Введите параметр: ";
+
+      cin >> AnyValue;//поток ввода читает число, то есть 2.2sdw или 22 sdsx ds --> в переменную 22
+                      //ws22 --> cin.fail()-true ошибка, повторяем цикл
+
+      if (cin.fail()) // если предыдущее извлечение оказалось неудачным
+                      //fail()-true, если ошибка
+
+      {//в этом блоке обработка ошибки
+        cin.clear(); // то возвращаем cin в 'обычный' режим работы, очищаем
+        cin.ignore(32767,'\n'); // и удаляем значения предыдущего ввода из входного буфера 32767-все и \n
+        cout << "\n!!!!!<<Ошибка ввода>>!!!!!" << endl;//сообщение об ошибке
+
+      }else
+        if(cin.good())
+        {
+          cin.ignore(32767,'\n');
+          break;}
+  }
+  cout << "Было введено значение: " << AnyValue << "\n";
+
+  return AnyValue;//возвращаемое значение
+}
+
+//#include <QCoreApplication>
+
+////директивы препроцессору
+//#include <iostream>//поточный модуль
+//#include <stdio.h>
+//#include <math.h>//для математики
+
+////используем пространство имен std, можно не писать запись std::cout
+//using namespace std;
+
+//определения/назначения
+#define MainProgram true
+//#define TaskFirst true//задача 1
+//#define TaskTwo true//задача 2
+//#define TaskTree true//задача 3
+
+//основной блок/функция программы без аргументов
+int main(int argc, char *argv[])
+{//блок стейтменов начало
+#ifndef MainProgram
+  QCoreApplication a(argc, argv);
+
+  return a.exec();
+#endif
+
+#ifndef TaskFirst
+  cout << "Задача 1.\nВычисление скорости прямолиненйного движения.\n";
+  cout << "(по расстоянию и времени.)\n";
+  cout << endl;
+
+/*
+ * используемые переменные Distance TimeMove SpeedMove
+ * Speed=Distance/Time
+ * тип данных float, проверка ввода
+*/
+  float SpeedMove, TimeMove, Distance;//task 1 вычисление скорости прямолиненого движения
+  Distance=0;TimeMove=0;SpeedMove=0;//обнуляемся
+
+  cout << "Введите расстояние (м)\n";
+  Distance = EnterData();//первая переменная
+
+  cout << "Введите время (с)\n";
+  TimeMove = EnterData();//вторая переменная
+
+  //если вторая переменная не нуль, делаем расчет
+  if(TimeMove != 0){
+    SpeedMove = Distance / TimeMove;
+  }else{
+    cout << "На нуль не делим!\n";
+    exit(-1);//выход из программы с ошибкой
+  }
+
+//и выводим результат
+  cout << "Задача 1.\n";
+  cout << "Были введены следующие данные, дистанция: " << Distance << " (м) время: " << TimeMove << " (с)\n";
+  cout.setf(ios::fixed);cout.precision(3);//точность до 3 знаков после запятой
+  cout << "Результат: " << SpeedMove << " м/с";
+  cout << endl;
+#endif
+/**************************************************************************************************************/
+
+#ifndef TaskTwo
+
+  cout << "\nЗадача 2.\nВычислить ускорение (по начальной скорости, конечной скорости и времени).\n";
+  cout << endl;
+  /*
+   * используемые переменные SpeedPoint1, SpeedPoint2, TimePoint1, TimePoint2, AccelerationAll
+   * AccelerationAll = (SpeedPoint2-SpeedPoint1)/(TimePoint2-TimePoint1)
+   * тип данных float, проверка ввода
+  */
+  float SpeedPoint1, SpeedPoint2, TimePoint1, TimePoint2, AccelerationAll;//task 2 вторая задача
+  //обнуляемся
+
+  SpeedPoint1=0;SpeedPoint2=0;TimePoint1=0;TimePoint2=0;
+  cout << "Введите начальную скорость (м/с)\n";
+  SpeedPoint1 = EnterData();//первая переменная
+  cout << "Введите конечную скорость (м/с)\n";
+  SpeedPoint2 = EnterData();
+  cout << "Введите время начальное (с)\n";
+  TimePoint1 = EnterData();
+  cout << "Введите время конечное (с)\n";
+  TimePoint2 = EnterData();//четвертая переменная
+
+  //если знаменатель не нуль, делаем расчет
+  if(TimePoint2 != TimePoint1){
+    AccelerationAll = (SpeedPoint2-SpeedPoint1)/(TimePoint2-TimePoint1);
+  }else{
+    cout << "На нуль не делим!\n";
+    exit(-1);//выход из программы с ошибкой
+  }
+
+  //и выводим результат
+    cout << "Задача 2.\n";
+    cout << "Были введены следующие данные\nначальная скорость " << SpeedPoint1 << " (м/с)\n"
+         << "конечная скорость " << SpeedPoint2 << " (м/с)\n"
+         << "начальное время " << TimePoint1 << " (c)\n"
+         << "конечное время " << TimePoint2 << " (c)\n";
+
+    cout.setf(ios::fixed);cout.precision(3);//точность до 3 знаков после запятой
+    cout << "Результат: " << AccelerationAll << " ускорение м2/с ";
+    if(AccelerationAll < 0){
+      cout << "Торможение!\n";
+    }else{
+      cout << "Разгон!\n";
+    }
+    cout << endl;
+#endif
+/**************************************************************************************************************/
+#ifndef TaskTree
+  cout << "\nЗадача 3.\nВычислить радиус круга (по длине его окружности)\n";
+  cout << endl;
+  /* используемые переменные CircleSquare CircleLong CircleRadius
+   * <<R=L/(2*Pi)>>
+   * тип данных float, проверка ввода
+  */
+
+  float CircleSquare, CircleLong, CircleRadius;
+  float PiConst = M_PI;// постоянная Pi
+
+  //обнуляемся
+  CircleSquare=0; CircleLong=0; CircleRadius=0;
+  cout << "Введите радиус (м)\n";
+  CircleLong = EnterData();//длина круга
+  CircleRadius = CircleLong / (2 * PiConst);
+
+  //и выводим результат
+    cout << "Задача 3.\n";
+    cout << "Были введены следующие данные\nдлина окружности " << CircleLong << " (м)\n";
+
+  cout.setf(ios::fixed);cout.precision(6);//точность до 6 знаков после запятой
+  cout << "Результат: " << CircleRadius << "(радиус) м " << " площадь круга "
+       << (PiConst * (CircleRadius*CircleRadius)) << " (м2)\n";
+#endif
+
+  return 0;
+}//блок стейтменов окончание
+
 #endif
