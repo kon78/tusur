58cd2e824bbe9115b7164c536e85bf9762d8be64
kon78 <misterqjr@mail.ru>
2025-03-11 20:26:58 +0300

    modified main.cpp added new files: task1.cpp header.h

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -1,29 +1,25 @@
+//TODO git ветка lab2, слить с веткой master!!!! LOCALE?
+//TODO округление весов до целых наверно
+
 /*
 * Лабораторная работа No2
 * Исследование ввода-вывода типов данных стандартной библиотеки С++
 * Типы данных
 * Developer: Половян КА 318OOP/C++
-* git ветка lab2, слить с веткой master!!!!
+* Windows 10, LOCALE 866
 */
+#if 1
 #include <iostream>// подключаем библиотеку ввода/вывода
+#include <task1.cpp>
 
-#if 1//Задание 1.“Калькулятор для расчета идеального веса“.
-#define size 80
+//Задание 1.“Калькулятор для расчета идеального веса“.
+
 using namespace  std;
 int main(int argc, char *argv[])
 {
+
 //используемые переменные camelCase
 
-int patOldYears;//возвраст пациента (0-115)
-int patMass;//вес пациента (1-125)
-int patMassAlg;//вес пациента по расчету
-bool patSex;//пол пациента (0-1)
-int patHigh;//рост пациента (50-250)
-char algProg;//выбор варианта расчета
-float index;//индекс массы тела
-float normMass1, normMass2;//нормы весов
-float indPatHigh;//рост пациента в метрах
-bool errPatData;//неверные данные, не подходят для условий задачи, патологии
   cout << "Программа Калькулятор для расчета идеального веса.\n";
   cout << "Для расчета пользователю необходимо ввести следующие данные:\n";
   cout << "\nвозвраст пациента, полных лет: ";
@@ -40,11 +36,7 @@ bool errPatData;//неверные данные, не подходят для у
   cin.clear();  cin >> algProg;
 
 /* проверка введенных данных
- * тут конечно все относительно
- * возраст 46, вес 35, рост 184 - либо патология либо узник Дахау и т.д.
- * возраст 15, вес 150, рост 165 - явное заболевание щетовидной к примеру
- * возраст 45, вес 40, рост 110 - карлик, либо инвалид (без ног)
- * поэтому за основу взял таблицу из задания
+ * за основу взял таблицу из задания
 */
   errPatData = false;
   if(
@@ -65,137 +57,53 @@ bool errPatData;//неверные данные, не подходят для у
     exit(1);
   }
 
-//расчеты
-  if(algProg == 'a'){
-//    algProgText = "формула Брока";
-    const int brokConst = 100;
-    const float brokMaleCoef = 0.9;
-    const float brokFemaleCoef = 0.89;
+  /*
+   * формула Брока
+   * мужчины вес = (рост см - 100) * 0.9
+   * женщины вес = (рост см - 100) * 0.85
+  */
 
-/*
- * формула Брока
- * мужчины вес = (рост см - 100) * 0.9
- * женщины вес = (рост см - 100) * 0.85
-*/
-    if(patSex == true){//male
-      if(patHigh > 50 && patHigh <= 250)//ограничения на рост 50-250 см
-        patMassAlg = (patHigh - brokConst) * brokMaleCoef;
-      else{
-        cout << "Указанный рост вне допустимого диапазона 50 - 250 см!";
-        exit(1);//выходим из программы с сообщением в систему
-      }
-    }else if(patSex == false){
-      patMassAlg = (patHigh - brokConst) * brokFemaleCoef;
-    }
+//расчеты общий расчет по имеющимся данным
+  if(algProg == 'a'){
+    ((patSex)?brokCoef = 0.9:brokCoef = 0.89);//male female
+    patMassAlg = patMassBroke();
+    answer();
+    answerBroke(patMassAlg,patMass);//ответ на экран
+  /*
+   * вес по индексу массы тела
+   * индекс = вес, кг/(рост, м)2
+   * вес,кг = индекс * рост,м2
+  */
 
   }else if(algProg == 'b'){
-/*
- * вес по индексу массы тела
- * индекс = вес, кг/(рост, м)2
- * вес,кг = индекс * рост,м2
-*/
-    if(patHigh != 0){
-//      float indPatHigh;//рост пациента в метрах
-      indPatHigh = (float)patHigh / 100;
+    if(patHigh != 0){//проверка на 0, на всякий случай
+      indPatHigh = (float)patHigh / 100;//перевод в метры
       index = (float)patMass / (indPatHigh * indPatHigh);
-//      cout << indPatHigh << "\n";
+      answer();
+      answerIndMass(index);//ответ на экран
     }else{
-      cout << "Рост пациента не может быть 0 сантиметров!";
-      exit(1);
+      cout << "Рост пациента не может быть нулевым!\n";
     }
-
+    /*
+     * вес по индексу массы тела и возрасту
+    */
   }else if(algProg == 'c'){
-/*
- * вес по индексу массы тела и возрасту
-*/
-    if(patOldYears <=19 || patOldYears >=24){
-        ((patSex)?indexOldYears = 21.4:indexOldYears = 19.5);
-      }else if(patOldYears <=25 || patOldYears >=34){
-        ((patSex)?indexOldYears = 21.6:indexOldYears = 23.2);
-      }else if(patOldYears <=35 || patOldYears >=44){
-        ((patSex)?indexOldYears = 22.9:indexOldYears = 23.4);
-      }else if(patOldYears <=45 || patOldYears >=54){
-        ((patSex)?indexOldYears = 25.8:indexOldYears = 25.2);
-      }else if(patOldYears > 55){
-        ((patSex)?indexOldYears = 26.6:indexOldYears = 27.3);
+      cout << "algorithm c" << endl;
+    selYears(patSex,patOldYears);//индекс в зависимости от возраста и пола пациента
+    answer();
+    answerIndMassOldYear(indexOldYears);
+      if((patMass - normMass1) > 0){//избыточный вес
+        cout << "Вес необходимо сбросить на " << (patMass - normMass1) << " килограмм.";
+        cout << "\nНормальный вес пациента " << normMass1 << " килограмм." << endl;
+      }else if((patMass - normMass1) < 0){//недостаточный вес
+        cout << "Вес необходимо набрать на " << (normMass1 - patMass) << " килограмм.";
+        cout << "\nНормальный вес пациента " << normMass1 << " килограмм." << endl;
+      }else{
+        cout << "Вес пациента в пределах нормы!\n";
       }
-  }else{
+    }else{
     cout << "\nБыло введено значение отличное от a, b, c!!!\n";
-
-
-  //вывод результатов
-  cout << "\nБыли введены следующие данные:\n";
-  cout << "Пациент: " << ((patSex)?" Мужчина ":" Женщина ")
-       << " возраст " << patOldYears << " лет "
-       << " вес пациента " << patMass << " килограмм "
-       << " рост пациента " << patHigh << " сантиметров\n";
-
-  //вывод результатов формула Брока
-  if(algProg == 'a'){
-    cout << "Вариант расчета " << "формула Брока " << "вес " << patMassAlg << " килограмм.\n";
-    if(patMassAlg < patMass){
-      cout << "Вес необходимо сбросить на " << (patMass - patMassAlg) << " килограмм\n";
-    }else if(patMass < patMassAlg){
-      cout << "Вес необходимо набрать на " << (patMassAlg - patMass) << " килограмм \n" ;
-    }
-
-  //вывод результатов по индексу массы тела
-  }else if(algProg == 'b'){
-    cout << "индекс массы тела " << index << "\n";
-    //пороги индекса массы тела
-    if(index < 16){
-      cout << "\nВыраженный дифицит массы тела.";
-    }else if(index > 16 && index < 18.5){
-      cout << "\nНедостаточная (дефицит) масса тела.";
-    }else if(index > 18.5 && index < 25){
-      cout << "\nНорма.";
-    }else if(index > 25 && index < 30){
-      cout << "\nИзбыточная масса тела (предожирение).";
-    }else if(index > 30 && index < 35){
-      cout << "\nОжирение первой степени";
-    }else if(index > 35 && index < 40){
-      cout << "\nОжирение второй степени";
-    }else if(index > 40){
-      cout << "\nОжирение третьей степени (морбидное)";
-    }
-  cout << "\nВариант расчета " << "по индексу массы тела (индекс массы тела " << index
-       << ") вес " << patMass << " килограмм.\n";
-  //вывод результатов по индексу массы тела и возрасту
-  //18.5 <--> 25 норма
-  const float indexNorm1 = 18.5;
-  const float indexNorm2 = 25;
-  normMass1 = indexNorm1 * (indPatHigh * indPatHigh);
-  normMass2 = indexNorm2 * (indPatHigh * indPatHigh);
-
-  //набрать вес
-  if(index < 18.5){
-    cout << "Вес необходимо набрать на " << (normMass1 - patMass) << " килограмм.";
-  }else if(index > 25){
-  //сбросить вес
-    cout << "Вес необходимо сбросить на " << (patMass - normMass2) << " килограмм.";
-  }else{
-    cout << "Вес пациента в пределах нормы!\n";
-  }
-  }else if(algProg == 'c'){
-    cout << "индекс массы тела " << index << " возраст пациента " << patOldYears
-         << " пол пациента " << ((patSex)?" Мужчина ":" Женщина ") << end;
-
-    if(patOldYears <=19 || patOldYears >=24){
-        if(patSex){
-          indexOldYears
-        }else if(!patSex){
-
-        }
-      }else if(patOldYears <=25 || patOldYears >=34){
-
-      }else if(patOldYears <=35 || patOldYears >=44){
-
-      }else if(patOldYears <=45 || patOldYears >=54){
-
-      }else if(patOldYears > 55){
-
-      }
-
+//}
 
   }
   return 0;
