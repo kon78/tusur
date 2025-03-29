34567f1a8d957e261ef13b5303bbba1caa2ad0db
kon78 <misterqjr@mail.ru>
2025-03-29 18:41:12 +0300

    modified files task1

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -8,22 +8,28 @@
 * OS: Windows 10
 * locale (кодировка): UTF-8
 */
-#define Unit 1
 
 #include <iostream>
 #include "task1.h"//заголовочный файл для задачи 1
+
+#define Unit 1
+
+
 using namespace std;
-
-//enum{max_cards=52, count_tuz=4, percent_per=100};
-
 int main(int argc, char *argv[])
 {
 #if Unit == 1
-
+//  double probability_cards; //ответ
 cout << "Задание 1" << endl;
 // Вызов функции задания 1 из отдельного файла (*.h ) или модуля (*.срр + *.h)
-  probability_cards = GameCards();
-  cout << "Вероятность вынуть из колоды (52 карты) туза (без масти) будет равна " << probability_cards << "%\n";
+if(max_cards != 0){
+  cout << "Вероятность вынуть из колоды (" << max_cards << " карты) туза (без масти) будет равна "
+       << GameCards() << "%\n";
+}else{
+  cout << "Error! Деление на 0!\n";
+  exit(progr_err);
+}
+
 #elif Unit == 2
 std::cout << "Задание 2" << std::endl;
 // Вызов функции задания 2 из отдельного файла (*.h ) или модуля (*.срр + *.h)
@@ -35,6 +41,5 @@ std::cout << "Задание 3" << std::endl;
 // Вызов функции задания 4 из отдельного файла (*.h ) или модуля (*.срр + *.h)
 //...
 #endif
-
   return 0;
-}
+  }
--- Lab_3/task1.h
+++ Lab_3/task1.h
@@ -1,6 +1,6 @@
 #ifndef TASK1_H
 #define TASK1_H
 double GameCards();// объявление функции
-double probability_cards; //ответ
-//enum{max_cards=52, count_tuz=4, percent_per=100};
+enum{max_cards=52, count_tuz=4, percent_per=100,progr_err=-1};
+//enum{progr_err=-1};
 #endif // TASK1_H
